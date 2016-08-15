6c13535eaab639a70f42de0b4b563b98b726f0e3
Ronald van Haren <r.vanharen@esciencecenter.nl>
2016-08-15 14:16:25 +0200

    define basic project structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyAccess",
+    version = "0.0.1",
+    author = "Ronald van Haren, Oscar Martinez Rubi",
+    author_email = "r.vanharen@esciencecenter.nl",
+    description = (""),
+    license = "Apache 2.0",
+    keywords = "VOEvent, FRBCAT",
+    url = "https://github.com/AA-ALERT/pyAccess",
+    packages=['pyAccess'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved ::Apache Software License",
+    ],
+)
+
